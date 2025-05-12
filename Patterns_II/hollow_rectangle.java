0f39e215a08d84e091054b2d3fe177aeb871f544
Abhay Kumar Verma <2k23.cscys2313035@gmail.com>
2025-05-12 21:41:39 +0530

    Create hollow_rectangle.java
    Print a hollow rectangle having variable no. of rows and columns.

---
+++ Patterns_II/hollow_rectangle.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Patterns_II {
+     public static void hollow_rectangle(int row, int col){
+          // outer loop for rows
+          for(int i=1;i<=row;i++){
+               // inner loop for columns
+               for(int j=1;j<=col;j++){
+                    // condition for first and last row or first and last column
+                    if(i==1 || i==row || j==1 || j==col){
+                         System.out.print("* ");
+                    }else{
+                         System.out.print("  ");
+                    }
+               }
+               System.out.println();
+          }
+     }
+public static void main(String[] args){
+          Scanner sc =new Scanner(System.in);
+          System.out.println("Enter the number of rows and columns for hollow rectangle: ");
+          int a = sc.nextInt();
+          int b = sc.nextInt();
+          hollow_rectangle(a,b);
+          sc.close();
+}
+
+  Author: Abhay Kumar Verma (Cybersecurity)
