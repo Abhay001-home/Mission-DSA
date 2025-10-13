4c7f714227055500fbc61db5d22fc056dae4bd71
Abhay Kumar Verma <av354947@gmail.com>
2025-10-14 01:26:13 +0530

    Upward Pointed Triangle

---
+++ Patterns_II/upWard.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class upWard {
+     public static void leftToRightUpward(int row, int col) {
+          // outer loop for rows
+          for (int i = 1; i <= row; i++) {
+               // inner loop for columns
+               for (int j = 1; j <= col; j++) {
+                    // condition for first and last row or first and last column
+                    if (i < j) {
+                         System.out.print(" ");
+                    } else {
+                         System.out.print("*");
+                    }
+               }
+               System.out.println();
+          }
+     }
+
+     public static void main(String args[]) {
+          Scanner sc = new Scanner(System.in);
+          int a = sc.nextInt();
+          int b = sc.nextInt();
+          leftToRightUpward(a, b);
+          sc.close();
+     }
+}
+
+// Author: Abhay Kumar Verma
