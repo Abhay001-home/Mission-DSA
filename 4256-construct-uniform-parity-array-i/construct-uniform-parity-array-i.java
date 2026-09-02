907164f17448bae6e2fde7cc6ffbe12cfcf42c46
Abhay Kumar Verma <av354947@gmail.com>
2026-09-03 01:44:53 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (73.96%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.java
@@ -1,18 +1,5 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int oddCount = 0;
-        
-        for (int num : nums1) {
-            if (num % 2 != 0) {
-                oddCount++;
-            }
-        }
-        
-        int n = nums1.length;
-        
-        boolean canMakeAllOdd = (oddCount > 0);
-        boolean canMakeAllEven = (oddCount != 1);
-        
-        return canMakeAllOdd || canMakeAllEven;
+        return true;   
     }
 }
