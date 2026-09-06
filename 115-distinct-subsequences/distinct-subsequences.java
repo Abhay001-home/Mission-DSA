03693dc3bd88e85ac6ee5b1b299dcca08f0b0f2b
Abhay Kumar Verma <av354947@gmail.com>
2026-09-07 00:15:22 +0530

    Time: 20 ms (53.25%) | Memory: 66.6 MB (6.38%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,34 +1,25 @@
-import java.util.Arrays;
-
 class Solution {
-    private long soln(String s, String t, int m, int n, long[][] dp) {
-        if (n == 0) {
-            return 1;
-        }
-        if (m == 0) {
-            return 0;
-        }
-
-        if (dp[m][n] != -1) {
-            return dp[m][n];
-        }
-
-        if (s.charAt(m - 1) == t.charAt(n - 1)) {
-            return dp[m][n] = soln(s, t, m - 1, n - 1, dp) + soln(s, t, m - 1, n, dp);
-        } else {
-            return dp[m][n] = soln(s, t, m - 1, n, dp);
-        }
-    }
-
     public int numDistinct(String s, String t) {
         int m = s.length();
         int n = t.length();
         
         long[][] dp = new long[m + 1][n + 1];
-        for (long[] row : dp) {
-            Arrays.fill(row, -1);
+        
+        // Base case: an empty t can be formed by any prefix of s in 1 way
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = 1;
         }
-
-        return (int) soln(s, t, m, n, dp);
+        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        
+        return (int) dp[m][n];
     }
 }
