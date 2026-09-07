80295f4efe9637269486fc0f78fe3835054b662c
Abhay Kumar Verma <av354947@gmail.com>
2026-09-08 00:40:42 +0530

    Time: 6 ms (43.62%) | Memory: 44.3 MB (22.34%) - LeetSync

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.java
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.java
@@ -5,30 +5,30 @@ class Solution {
     //prev[n] = last time when we saw this nth character (1-based indexing)
     int[] prev; 
     
-    int solve(int n) {
-        if(n == 0)
-            return 1;
-        if(dp[n] != -1)
-            return dp[n];
-        
-        int total = (int)(2L * solve(n-1) % M);
-        if(prev[n] != 0) {
-            int duplicates = solve(prev[n] - 1);
-            total = (total - duplicates + M) % M;
-        }
-        return dp[n] = total;
-    }
-
     public int distinctSubseqII(String s) {
+        // Bottom Up with duplicate handling
         int n = s.length();
         Arrays.fill(dp, -1);
         prev = new int[n+1];
         int[] lastSeen = new int[26];
+        
         for(int i = 1; i <= n; i++) {
             int idx = s.charAt(i-1) - 'a';
             prev[i] = lastSeen[idx];
             lastSeen[idx] = i;
         }
-        return (solve(n) - 1 + M) % M;
+        
+        dp[0] = 1; //if(n == 0) return 1;
+        
+        for(int i = 1; i <= n; i++) {
+            int total = (int)(2L * dp[i-1] % M);
+            if(prev[i] != 0) {
+                int duplicates = dp[prev[i] - 1];
+                total = (total - duplicates + M) % M;
+            }
+            dp[i] = total;
+        }
+        
+        return (dp[n] - 1 + M) % M;
     }
 }
